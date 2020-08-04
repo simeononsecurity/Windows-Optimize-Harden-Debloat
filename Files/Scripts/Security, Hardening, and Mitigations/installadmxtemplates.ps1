b6c579c8f355e783b1394a85f33dc65b16ba2597
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-08-04 18:04:56 -0500

    Update installadmxtemplates.ps1

--- Files/Scripts/Security, Hardening, and Mitigations/installadmxtemplates.ps1
+++ Files/Scripts/Security, Hardening, and Mitigations/installadmxtemplates.ps1
@@ -1,3 +1,1 @@
-takeown /f %windir%\Policydefinitions /r /a
-icacls %windir%\PolicyDefinitions /grant Administrators:(OI)(CI)F /t
-start-job -ScriptBlock {copy-item -Path .\Files\PolicyDefinitions\* -Destination C:\Windows\PolicyDefinitions -Force -Recurse -ErrorAction SilentlyContinue}
+start-job -ScriptBlock {takeown /f %windir%\Policydefinitions /r /a; icacls %windir%\PolicyDefinitions /grant Administrators:(OI)(CI)F /t; copy-item -Path .\Files\PolicyDefinitions\* -Destination C:\Windows\PolicyDefinitions -Force -Recurse -ErrorAction SilentlyContinue}
