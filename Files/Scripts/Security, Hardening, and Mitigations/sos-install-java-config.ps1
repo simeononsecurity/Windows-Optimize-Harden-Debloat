788931dd68f75c5d448b4deed969ec8204c1b239
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-08-06 23:56:04 -0500

    Update sos-install-java-config.ps1

--- Files/Scripts/Security, Hardening, and Mitigations/sos-install-java-config.ps1
+++ Files/Scripts/Security, Hardening, and Mitigations/sos-install-java-config.ps1
@@ -13,7 +13,7 @@ If (Test-Path -Path "C:\Windows\Sun\Java\Deployment\"){
 }Else {
     Mkdir "C:\Windows\Sun\Java\Deployment\"
     Mkdir "C:\temp\JAVA"
-    Copy-Item -Path .\Files\deployment.config -Destination "C:\Windows\Sun\Java\Deployment\" -Force
-    Copy-Item -Path .\Files\deployment.properties -Destination "C:\temp\JAVA\" -Force
-    Copy-Item -Path .\Files\exception.sites -Destination "C:\temp\JAVA\" -Force
+    Copy-Item -Path .\Files\"JAVA Configuration Files"\deployment.config -Destination "C:\Windows\Sun\Java\Deployment\" -Force
+    Copy-Item -Path .\Files\"JAVA Configuration Files"\deployment.properties -Destination "C:\temp\JAVA\" -Force
+    Copy-Item -Path .\Files\"JAVA Configuration Files"\exception.sites -Destination "C:\temp\JAVA\" -Force
 }
