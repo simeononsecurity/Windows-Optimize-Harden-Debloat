4d408f87abb4220e214138ba587b586be092c5e6
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-08-07 00:09:41 -0500

    Update sos-.net-4-stig.ps1

--- Files/Scripts/Security, Hardening, and Mitigations/sos-.net-4-stig.ps1
+++ Files/Scripts/Security, Hardening, and Mitigations/sos-.net-4-stig.ps1
@@ -5,29 +5,25 @@
 
 
 Remove-Item "HKLM:\Software\Microsoft\StrongName\Verification" -Recurse -Force
-$net32 = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\"
-$net64 = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\"
 
-If (Test-Path -Path $net32){
+If (Test-Path -Path C:\Windows\Microsoft.NET\Framework\v4.0.30319){
     Write-Host ".Net 32-Bit is Installed"
-    $net32\caspol.exe -m -lg
-    $net32\caspol.exe -m -rs
-    $net32\caspol.exe -q -f -execution on -m -a -pp on -security on  
+    C:\Windows\Microsoft.NET\Framework\v4.0.30319\caspol.exe -q -f -pp on 
+    C:\Windows\Microsoft.NET\Framework\v4.0.30319\caspol.exe -m -lg
     New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\.NETFramework\" -Name "AllowStrongNameBypass" -PropertyType "DWORD" -Value "0"
     New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319\" -Name "SchUseStrongCrypto" -PropertyType "DWORD" -Value "1"
-    Copy-Item -Path .\Files\".Net Configuration Files"\machine.config -Destination $net32\Config -Force 
+    Copy-Item -Path .\Files\".Net Configuration Files"\machine.config -Destination C:\Windows\Microsoft.NET\Framework\v4.0.30319\Config -Force 
 }Else {
     Write-Host ".Net 32-Bit Is Not Installed"
 }
-If (Test-Path -Path $net64){
+If (Test-Path -Path C:\Windows\Microsoft.NET\Framework64\v4.0.30319){
     Write-Host ".Net 64-Bit Is Installed"
-    $net64\caspol.exe -m -lg
-    $net64\caspol.exe -m -rs
-    $net64\caspol.exe -q -f -execution on -m -a -pp on -security on 
+    C:\Windows\Microsoft.NET\Framework64\v4.0.30319\caspol.exe -q -f -pp on 
+    C:\Windows\Microsoft.NET\Framework64\v4.0.30319\caspol.exe -m -lg
     New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\.NETFramework\" -Name "AllowStrongNameBypass" -PropertyType "DWORD" -Value "0"
     New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\" -Name "AllowStrongNameBypass" -PropertyType "DWORD" -Value "0"
     New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319\" -Name "SchUseStrongCrypto" -PropertyType "DWORD" -Value "1"
-    Copy-Item -Path .\Files\".Net Configuration Files"\machine.config -Destination $net64\Config -Force 
+    Copy-Item -Path .\Files\".Net Configuration Files"\machine.config -Destination C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Config -Force 
 }Else {
     Write-Host ".Net 64-Bit Is Not Installed"
 }
