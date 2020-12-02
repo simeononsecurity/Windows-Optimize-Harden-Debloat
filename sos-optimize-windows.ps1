2732b9591934a6ea93df87a3d037e9999b265ac1
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-12-02 13:24:30 -0600

    Update sos-optimize-windows.ps1

--- sos-optimize-windows.ps1
+++ sos-optimize-windows.ps1
@@ -1868,7 +1868,7 @@ Foreach ($gpocategory in Get-ChildItem "$(Get-Location)\Files\GPOs") {
     Foreach ($gpo in (Get-ChildItem "$(Get-Location)\Files\GPOs\$gpocategory")) {
         $gpopath = "$gposdir\$gpocategory\$gpo"
         Write-Output "Importing $gpo"
-        .\Files\LGPO\LGPO.exe /g $gpopath | Out-Null
+        .\Files\LGPO\LGPO.exe /g $gpopath > $null
     }
 }
 
@@ -1876,7 +1876,7 @@ Add-Type -AssemblyName PresentationFramework
 $Answer = [System.Windows.MessageBox]::Show("Reboot to make changes effective?", "Restart Computer", "YesNo", "Question")
 Switch ($Answer)
 {
-    "Yes"   { Write-Warning "Restarting Computer in 15 Seconds"; Start-sleep -seconds 15; Restart-Computer -Force }
-    "No"    { Write-Warning "A reboot is required for all changed to take effect" }
+    "Yes"   { Gpupdate /force /boot; Write-Warning "Restarting Computer in 15 Seconds"; Start-sleep -seconds 15; Restart-Computer -Force }
+    "No"    { Gpupdate /force ; Write-Warning "A reboot is required for all changed to take effect" }
     Default { Write-Warning "A reboot is required for all changed to take effect" }
 }
