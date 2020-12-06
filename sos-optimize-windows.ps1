fdc486d78b9dfa191d9b2dfc6a69326c1ddf693a
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-12-06 08:57:49 +0000

    Image Cleanup

--- sos-optimize-windows.ps1
+++ sos-optimize-windows.ps1
@@ -645,9 +645,6 @@ Start-Job -Name "Disable Telemetry and Services" -ScriptBlock {
     net stop "VSStandardCollectorService150"
     Set-Service  "VSStandardCollectorService150" -StartupType Disabled
     #General Optmizations
-    #Delete "windows.old" folder
-    #Cmd.exe /c Cleanmgr /sageset:65535 
-    Cmd.exe /c Cleanmgr /sagerun:65535
 }
 
 Start-Job -Name "Enable Privacy and Security Settings" -ScriptBlock {
@@ -1641,6 +1638,34 @@ Start-Job -Name "Microsoft .Net Framework 4 STIG Script" -ScriptBlock {
 
     #Vul ID: V-30937	   	Rule ID: SV-40979r3_rule	   	STIG ID: APPNET0064	  
     #FINDSTR /i /s "NetFx40_LegacySecurityPolicy" c:\*.exe.config 
+}
+
+Start-Job -Name "Image Cleanup" -ScriptBlock {
+    #Delete "windows.old" folder
+    #Cmd.exe /c Cleanmgr /sageset:65535 
+    Cmd.exe /c Cleanmgr /sagerun:65535
+    Write-Verbose "Removing .tmp, .etl, .evtx, thumbcache*.db, *.log files not in use"
+    Get-ChildItem -Path c:\ -Include *.tmp, *.dmp, *.etl, *.evtx, thumbcache*.db, *.log -File -Recurse -Force -ErrorAction SilentlyContinue | Remove-Item -ErrorAction SilentlyContinue
+    # Delete "RetailDemo" content (if it exits)
+    Write-Verbose "Removing Retail Demo content (if it exists)"
+    Get-ChildItem -Path $env:ProgramData\Microsoft\Windows\RetailDemo\* -Recurse -Force -ErrorAction SilentlyContinue | Remove-Item -Recurse -ErrorAction SilentlyContinue
+    # Delete not in-use anything in the C:\Windows\Temp folder
+    Write-Verbose "Removing all files not in use in $env:windir\TEMP"
+    Remove-Item -Path $env:windir\Temp\* -Recurse -Force -ErrorAction SilentlyContinue
+    # Clear out Windows Error Reporting (WER) report archive folders
+    Write-Verbose "Cleaning up WER report archive"
+    Remove-Item -Path $env:ProgramData\Microsoft\Windows\WER\Temp\* -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path $env:ProgramData\Microsoft\Windows\WER\ReportArchive\* -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path $env:ProgramData\Microsoft\Windows\WER\ReportQueue\* -Recurse -Force -ErrorAction SilentlyContinue
+    # Delete not in-use anything in your %temp% folder
+    Write-Verbose "Removing files not in use in $env:TEMP directory"
+    Remove-Item -Path $env:TEMP\* -Recurse -Force -ErrorAction SilentlyContinue
+    # Clear out ALL visible Recycle Bins
+    Write-Verbose "Clearing out ALL Recycle Bins"
+    Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+    # Clear out BranchCache cache
+    Write-Verbose "Clearing BranchCache cache"
+    Clear-BCCache -Force -ErrorAction SilentlyContinue
 }
 
 ##Firefox Config Import
