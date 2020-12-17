17014ba61168f76a1010c87259397ffee9cf947d
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-12-17 07:56:55 +0000

    Update sos-optimize-windows.ps1

--- sos-optimize-windows.ps1
+++ sos-optimize-windows.ps1
@@ -108,10 +108,9 @@ Start-Job -Name "Mitigations" -ScriptBlock {
     #Enable SEHOP
     Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" -Name "DisableExceptionChainValidation" -Type "DWORD" -Value 0 -Force
 
-    $key = "HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
-
     #Disable NetBIOS by updating Registry
     #http://blog.dbsnet.fr/disable-netbios-with-powershell#:~:text=Disabling%20NetBIOS%20over%20TCP%2FIP,connection%2C%20then%20set%20NetbiosOptions%20%3D%202
+    $key = "HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
     Get-ChildItem $key | foreach { 
     Write-Host("Modify $key\$($_.pschildname)")
     $NetbiosOptions_Value = (Get-ItemProperty "$key\$($_.pschildname)").NetbiosOptions
