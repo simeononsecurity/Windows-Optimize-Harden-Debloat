38166cb77688b4591bb1545f78b07d6e5f80447f
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-12-17 07:55:42 +0000

    Update sos-optimize-windows.ps1

--- sos-optimize-windows.ps1
+++ sos-optimize-windows.ps1
@@ -112,8 +112,7 @@ Start-Job -Name "Mitigations" -ScriptBlock {
 
     #Disable NetBIOS by updating Registry
     #http://blog.dbsnet.fr/disable-netbios-with-powershell#:~:text=Disabling%20NetBIOS%20over%20TCP%2FIP,connection%2C%20then%20set%20NetbiosOptions%20%3D%202
-    Get-ChildItem $key |
-    foreach { 
+    Get-ChildItem $key | foreach { 
     Write-Host("Modify $key\$($_.pschildname)")
     $NetbiosOptions_Value = (Get-ItemProperty "$key\$($_.pschildname)").NetbiosOptions
     Write-Host("NetbiosOptions updated value is $NetbiosOptions_Value")
