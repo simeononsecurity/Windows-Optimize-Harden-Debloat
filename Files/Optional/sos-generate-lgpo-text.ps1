8495eeea673c56941b97e8b25dd272f4fbd1adf3
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2022-04-23 20:07:11 +0000

    Delete Files/Optional directory

--- Files/Optional/sos-generate-lgpo-text.ps1
+++
@@ -1,5 +0,0 @@
-$GPOLIST = Get-ChildItem *.pol -recurse
-foreach ($item in $GPOLIST){
-    .\Files\LGPO\LGPO.exe /parse /m $item | add-content .\computer.txt
-    .\Files\LGPO\LGPO.exe /parse /u $item | add-content .\user.txt
-}
